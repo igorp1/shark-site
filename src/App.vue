4df41bcdd32f2302b72c756eb6f83dda6552c4c4
idp <igordepaula1@gmail.com>
2018-01-22 16:57:54 -0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <router-view/>
+</template>
+
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700');
+@import url('https://unpkg.com/purecss@1.0.0/build/pure-min.css');
+@import url('https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css');
+
+body {
+    font-family: 'Open Sans', sans-serif;
+    padding: 1em;
+}
+</style>
